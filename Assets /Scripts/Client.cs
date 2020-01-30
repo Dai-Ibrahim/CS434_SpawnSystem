9b108cc438f80692a84730a5e7fb07a6c0ee4fc6
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-01-29 18:51:58 -0700

    Submission Production Assignment

---
+++ Assets /Scripts/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+
+	public int Location;
+    public bool Formal;
+    public bool Woman;
+
+    void Start()
+    {
+        ClothesRequirements requirements = new ClothesRequirements();
+
+        requirements.Woman = Woman;
+        requirements.Location = Mathf.Max(Location);
+        requirements.Woman = Formal;
+        requirements.Formal = Formal;
+
+        ClothesFactory factory = new ClothesFactory(requirements);
+        IClothes v = factory.Create();
+        Debug.Log(v);
+    }
+
+    void Update()
+    {
+        
+    }
+}
